149f10a31551f9cb86e63293735b84acd46a4995
Janani <janani@github.com>
2018-03-07 10:27:02 -0500

    Deleted spaces and formatted Day0

--- src/Day0.java
+++ src/Day0.java
@@ -4,14 +4,11 @@ public class Day0 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String inputString= scan.nextLine();
+		String inputString = scan.nextLine();
 		scan.close();
-		
+
 		System.out.println("Hello, World.");
 		System.out.println(inputString);
-		
-		
 
 	}
-
 }
