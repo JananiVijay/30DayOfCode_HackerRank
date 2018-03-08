6ced6e3411e893204ec754c5efc1ef3c801c5f66
Janani <janani@github.com>
2018-03-08 18:06:13 -0500

    Day 1 - Data Types

---
+++ src/Day1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Day1 {
+	public static void main(String[] args) {
+	int i = 4;
+	double d = 4.0;
+	String s = "HackerRank";
+	
+	Scanner scan = new Scanner(System.in);
+	
+	 /* Declare second integer, double, and String variables. */
+	int j;
+	double e;
+	String s1;
+	
+	j = scan.nextInt();
+	e = scan.nextDouble();
+	s1 = scan.nextLine();
+	s1 = scan.nextLine();
+	
+	System.out.println(i + j);
+	System.out.println(d + e);
+	System.out.println(s + s1);
+	}
+}
